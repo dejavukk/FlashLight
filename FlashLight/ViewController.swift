b262fa6cea3ae63094ad53950fd025daf79a82ab
장준혁 <dejavukk1@gmail.com>
2018-09-04 16:44:05 +0900

    실제 기기에서 작동하는 플래시를 구현해보았습니다.

--- FlashLight/ViewController.swift
+++ FlashLight/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    // * 스위치 isOn, soundPlayer 변수 선언.
+    var isOn = false
+    var soundPlayer: AVAudioPlayer?
+    
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var flashImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        prepareSound()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +30,70 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // * 사운드 구현 함수
+    func prepareSound() {
+        
+        let path = Bundle.main.path(forResource: "switch.wav", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.play()
+        } catch {
+            // couldn't load file :(
+        }
+        
+    }
+    
+    // * 실제 기기에서 플래시 On/Off 구현.
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
+    // * 스위치를 Tap 했을 때 플래시 On/Off.
+    @IBAction func switchTapped(_ sender: UIButton) {
+        
+        isOn = !isOn
+        toggleTorch(on: isOn)
+        
+        soundPlayer?.play()
+        
+        if isOn == true {
+            switchButton.setImage(UIImage(named: "onSwitch"), for: .normal)
+            flashImageView.image = #imageLiteral(resourceName: "onBG")
+            
+        } else {
+            switchButton.setImage(#imageLiteral(resourceName: "offSwitch"), for: .normal)
+            flashImageView.image = UIImage(named: "offBG")
+        }
+        
+        /* 위에 스위치 키고 끄는 기능과 같은 표현이다.
+        
+         flashImageView.image = isOn ? #imageLiteral(resourceName: "onBG") : #imageLiteral(resourceName: "offBG")
+         switchButton.setImage(isOn ? #imageLiteral(resourceName: "onSwitch") : #imageLiteral(resourceName: "offSwitch"), for: .normal)
+         
+         */
+        
+    }
+    
 }
 
