fdf069e139abb3f0ec651fb04d84a6e98995fb95
장준혁 <dejavukk1@gmail.com>
2018-09-04 14:27:58 +0900

    Initial Commit

---
+++ FlashLight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlashLight
+//
+//  Created by JunHyuk on 2018. 9. 4..
+//  Copyright © 2018년 junhyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
